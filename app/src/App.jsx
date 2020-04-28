150b7ed0703b6d9c4354a3013cccc53361ffc87a
Mustafa Fahmy <mustafa_fahmy2010@hotmail.com>
2020-04-28 04:07:25 +0200

    google_analytics & words_bank

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import ReactGA from 'react-ga'; // Google Analytics
 import { GlobalProvider } from './core/globalState';
 import { Routes } from './core/routes';
 
-
+function initAnalytics() {
+  ReactGA.initialize('UA-164613143-1');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 function App() {
+  initAnalytics();
   return (
     <GlobalProvider>
       <center>
