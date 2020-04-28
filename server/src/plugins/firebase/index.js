eb8a0bbf619396ed36fdd70f19a86057e03773a2
Mustafa Fahmy <mustafa_fahmy2010@hotmail.com>
2020-04-28 04:07:25 +0200

    google_analytics & words_bank

--- server/src/plugins/firebase/index.js
+++ server/src/plugins/firebase/index.js
@@ -1,8 +1,32 @@
 // eslint-disable-next-line no-unused-vars
-function init(context) {
-  return {};
+const db = require('./db');
+
+function init() {
+  return class GameWords {
+    constructor(numWordsReq) {
+      this.numWordsReq = numWordsReq;
+      this.result = [];
+    }
+    generate() {
+      const wordsBank = db.init().wordsBank;
+      const max = wordsBank.length;
+      this.result = [];
+      while (this.numWordsReq > this.result.length) {
+        let generateNum = Math.floor(Math.random() * max); // (Exclusive MAX)
+        let genWord = wordsBank[generateNum];
+        if (this.numWordsReq > max || !this.result.includes(genWord)) this.result.push(genWord);
+      }
+    }
+    use3words() {
+      let words = this.result.slice(0, 3);
+      this.result.pop();
+      this.result.pop();
+      this.result.pop();
+      return words;
+    }
+  };
 }
 
 module.exports = {
-  init,
+  init
 };
