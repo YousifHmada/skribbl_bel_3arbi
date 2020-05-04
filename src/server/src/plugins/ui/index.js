e6ddb8e97ff57dfaeaaab01c76c89ab65bff8f28
yousifHmada <yousifhmada.moos@gmail.com>
2020-05-04 10:34:13 +0200

    :art: improve arch!

--- src/server/src/plugins/ui/index.js
+++ src/server/src/plugins/ui/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 // eslint-disable-next-line no-unused-vars
 function init(context) {
   return {
-    publicDir: path.resolve('./src/plugins/ui/react_app/build'),
+    publicDir: path.resolve('../app/build'),
     createRoomPage: path.resolve('./src/plugins/ui/pages/create_room.html'),
     roomPage: path.resolve('./src/plugins/ui/pages/room.html')
   };
